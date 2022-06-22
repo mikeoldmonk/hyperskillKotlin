38050a714a40cfce8d62b5f38512847bcaeae4ec
MikeWildkov <m.wildkov@gmail.com>
2022-06-22 14:23:33 +0300

    Initial commit

---
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -0,0 +1,13 @@
+package cinema
+
+fun main() {
+    println("Cinema:")
+    println("  1 2 3 4 5 6 7 8")
+    println("1 S S S S S S S S")
+    println("2 S S S S S S S S")
+    println("3 S S S S S S S S")
+    println("4 S S S S S S S S")
+    println("5 S S S S S S S S")
+    println("6 S S S S S S S S")
+    println("7 S S S S S S S S")
+}
