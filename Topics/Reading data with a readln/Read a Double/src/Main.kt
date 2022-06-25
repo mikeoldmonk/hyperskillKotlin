f577c3a5700dce9d6a6c28db1cd3cacc23f0308e
mikeoldmonk <mikeoldmonk@gmail.com>
2022-06-25 18:05:44 +0300

    Saturday 25.06.22

---
+++ Topics/Reading data with a readln/Read a Double/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    // put your code here
+    val a = readln().toDouble()
+    println(a)
+
+}
---
+++ Topics/Type conversion/Sum of Longs/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // write your code here
+
+    val num1 = readln()!!.toLong()
+    val num2 = readln()!!.toLong()
+
+    println(num1 + num2)
+
+}
