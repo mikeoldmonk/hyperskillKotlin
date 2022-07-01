0d2b8469e45f74f956af5aa6e886fe6d1a293570
mikeoldmonk <mikeoldmonk@gmail.com>
2022-07-01 22:50:11 +0300

    2/5 big test - temp, correct, but.....

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,14 +1,14 @@
 package cinema
 
 fun main() {
+    val seats = readln().toInt()
+    val rows = readln().toInt()
 
+    var sum = seats * rows
 
-    println("Enter the number of rows:") //рядов
-    val a = readln().toInt()
-
-    println("Enter the number of seats in each row:") //мест
-    val b = readln().toInt()
-
-    println("Total income:")
-    $200
+    if (sum <= 60) {
+        println(sum * 10)
+    } else {
+        println((rows.floorDiv(2) * 10 * seats) + ((rows - rows.floorDiv(2)) * 8 * seats))
+    }
 }
